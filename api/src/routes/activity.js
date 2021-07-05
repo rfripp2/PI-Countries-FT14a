c3c63af7ff183a2d2f92c6917c388acfb320a3e4
juan gonzalez venzano <juangonzalez465@gmail.com>
2021-07-05 15:13:37 -0300

    adding route for creating activity and vinculated to country

--- api/src/routes/activity.js
+++ api/src/routes/activity.js
@@ -6,13 +6,20 @@ const Op = Sequelize.Op;
 const router = Router();
 
 router.post("/", async (req, res) => {
-  const { name, dificulty, duration, season } = req.body;
-  Activity.create({
+  const { name, dificulty, duration, season, country } = req.body;
+  const countryFound = await Country.findOne({
+    where: {
+      name: country,
+    },
+  });
+  const activityCreated = await Activity.create({
     name,
     dificulty,
     duration,
     season,
   });
+  await countryFound.addActivity(activityCreated);
+  await activityCreated.setCountries(countryFound);
   return res.sendStatus(200);
 });
 
